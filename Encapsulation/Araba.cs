c33e08ec732752f5ca1e36795d2c8815649dfc35
Bilal Hantik <bilalhantik@gmail.com>
2025-05-25 22:04:22 +0300

    Add project files.

---
+++ Encapsulation/Araba.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Araba
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+
+        public string Renk { get; set; }
+
+        private int _KapıSayısı;  // Private bir kapsül
+        public int KapıSayısı 
+        {
+            get
+            {
+                return _KapıSayısı;
+            }
+            set 
+            {
+                if (value != 2 || value != 4) // Kapı sayısı 2 ve ya 4 girilmezse -1 atıyoruz.
+                {
+                   
+                    _KapıSayısı = -1;
+                    
+                }
+                else
+                {
+                    _KapıSayısı = value; // Eğer kapı sayısı doğru girilirse değeri döndürüyoruz.
+                }
+                
+            } 
+        }
+
+
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using Encapsulation;
+
+Araba araba = new Araba();
+
+
+
+Console.WriteLine("Lütfen Araç Bilgilerini Giriniz: ");
+
+Console.Write("Arabanın Markası: ");
+araba.Marka = Console.ReadLine();
+ 
+Console.Write("Arabanın Modeli: ");
+araba.Model = Console.ReadLine();
+
+Console.Write("Arabanın Rengi: ");
+araba.Renk = Console.ReadLine();
+
+
+Console.Write("Arabanın Kapı Sayısı: ");
+araba.KapıSayısı = int.Parse(Console.ReadLine());
+
+
+
+
+Console.Clear();
+Console.WriteLine("Arabanın Markası: " + araba.Marka);
+Console.WriteLine("Arabanın Modeli: " + araba.Model);
+Console.WriteLine("Arabanın Rengi: " +  araba.Renk);
+Console.WriteLine("Arabanın Kapı Sayısı : " +  araba.KapıSayısı);
